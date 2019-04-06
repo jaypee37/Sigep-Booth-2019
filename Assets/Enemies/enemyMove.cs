04f3a16eb22dcb25f37467ebba445eb375351692
jean paul <jeanpaun@andrew.cmu.edu>
2019-04-06 04:34:43 -0400

    enemy_attacking

--- Assets/Enemies/enemyMove.cs
+++ Assets/Enemies/enemyMove.cs
@@ -9,6 +9,7 @@ public class enemyMove : MonoBehaviour
     public Transform loc;
     private bool moveFinished = false;
     Vector3 goal;
+    Vector3 goal2;
     Vector3 turnV;
     ParticleSystem pSystem;
     public bool LockedOn;
@@ -16,6 +17,14 @@ public class enemyMove : MonoBehaviour
     public bool dead;
     public bool moving;
     public bool sentDeadReciept;
+    Transform tnew;
+    Animator animator;
+    public Transform loc2;
+    public bool enemyAttacking;
+    public EnemyAttackManager attackManager;
+    bool finishedAttacking = false;
+    public playerMove player;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -28,18 +37,43 @@ public class enemyMove : MonoBehaviour
         dead = false;
         moving = false;
         sentDeadReciept = false;
-        
+        animator = GetComponent<Animator>();
+        enemyAttacking = false;
+       
     }
     IEnumerator WaitForPosition()
     {
         print("enemy is moving");
-        yield return new WaitWhile(() => goal.magnitude > 2);
-        agent.destination = turnV;
-        moveFinished = true;
-        yield return new WaitForSeconds(2);
+        yield return new WaitWhile(() => goal.magnitude > 2);       
+        agent.destination = loc2.position;
+        yield return new WaitWhile(() => goal2.magnitude > 0.2f);
+        animator.SetBool("Run", false);
         agent.updatePosition = false;
         agent.updateRotation = false;
+        
         print("enemy made it");
+        yield return new WaitForSeconds(3);
+        enemyAttacking = true;
+        moveFinished = true;
+
+    }
+    IEnumerator WaitForDeath()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+        //transform.localScale = Vector3.zero;
+
+    }
+    IEnumerator WaitForAttack(int time)
+    {
+        yield return new WaitForSeconds(time);
+        animator.SetBool("Attack", false);
+        finishedAttacking = true;
+        FinishedAttack();
+        print("i finished attacking");
+        StopCoroutine(WaitForAttack(time));
+     
+
     }
 
     // Update is called once per frame
@@ -48,27 +82,33 @@ public class enemyMove : MonoBehaviour
         if (!moveFinished)
         {
             goal = transform.position - loc.position;
+            goal2 = transform.position - loc2.position;
         }
 
-        if(health == 0)
+        if (health == 0 && !dead)
         {
-            transform.localScale = Vector3.zero;
+            //transform.localScale = Vector3.zero;
+            animator.SetBool("Attack", false);
+            animator.SetBool("Run", false);
+            attackManager.enemyFinishedAttacking();
+            animator.SetTrigger("Death");
             dead = true;
             Destroy(pSystem);
-            Destroy(gameObject.GetComponent<CapsuleCollider>());
+            StartCoroutine(WaitForDeath());
 
         }
-        
-      
-        
+        if (moveFinished)
+        {
+            transform.position = loc2.position;
+        }
 
     }
-
     public void enemyMoveLoc()
     {
         moving = true;
         agent.updatePosition = true;
         agent.updateRotation = true;
+        animator.SetBool("Run",true);
         StartCoroutine(WaitForPosition());
         agent.destination = loc.position;
     }
@@ -86,9 +126,10 @@ public class enemyMove : MonoBehaviour
         transform.localScale = Vector3.zero;
     }
 
-    public void SetTurnVector(Vector3 turn)
+    public void SetTurnVector(Vector3 turn,GameObject t)
     {
         turnV = turn;
+        tnew = t.transform;
     }
     public void LockOn()
     {
@@ -103,5 +144,19 @@ public class enemyMove : MonoBehaviour
     public void takeDamage()
     {
         health -= 50;
+    }
+
+    public void Attack()
+    {
+        transform.LookAt(player.transform);
+        animator.SetBool("Run", false);
+        animator.SetBool("Attack",true);
+        int time = Random.Range(6, 9);
+        StartCoroutine(WaitForAttack(time));
+    }
+
+    public void FinishedAttack()
+    {
+        attackManager.enemyFinishedAttacking();
     }
 }   
---
+++ Assets/EnemyAttackManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackManager : MonoBehaviour
+{
+    enemyMove[] curEnemySet;
+    int setSize;
+    bool attacking;
+    bool sentAttackRequest;
+    bool enemyFinished;
+    enemyMove curAttackingEnemy;
+    bool waitingForAttack = false;
+    bool allDead = false;
+    bool activated = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        attacking = false;
+        sentAttackRequest = false;
+        enemyFinished = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activated)
+        {
+            if (CountDead() == setSize)
+            {
+                activated = false;
+            }
+
+            if (waitingForAttack && !allDead && activated)
+            {
+                if (enemyFinished)
+                {
+                    waitingForAttack = false;
+                    ChooseAttackingEnemy();
+                }
+            }
+            
+        }
+        
+    }
+
+    public void InitCurrentEnemySet(enemyMove[] enemySet, int size)
+    {
+        print("init");
+        curEnemySet = enemySet;
+        setSize = size;
+        ChooseAttackingEnemy();
+        allDead = false;
+        activated = true;
+    }
+
+    public enemyMove ChooseAttackingEnemy()
+    {
+        print("choosingcharacter");
+        int i = Random.Range(0, setSize);
+        print(i);
+        while (curEnemySet[i].dead)
+        {
+            i = (int)(Random.Range(0, setSize));
+        }
+        curAttackingEnemy = curEnemySet[i];
+        curEnemySet[i].Attack();
+        sentAttackRequest = true;
+        enemyFinished = false;
+        waitingForAttack = true;
+        return null;
+    }
+
+    public int CountDead()
+    {
+        int count = 0;
+        foreach( enemyMove e in curEnemySet)
+        {
+            if(e.dead)
+            {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    public void enemyFinishedAttacking()
+    {
+        enemyFinished = true;
+    }
+}
