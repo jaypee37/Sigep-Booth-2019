51c7b74b84f59d44186cba65bd515eaa511ca1ca
jean paul <jeanpaun@andrew.cmu.edu>
2019-04-07 16:56:28 -0400

    guitar_shit

--- Assets/Camera & UI/CameraFollow.cs
+++ Assets/Camera & UI/CameraFollow.cs
@@ -6,10 +6,15 @@ public class CameraFollow : MonoBehaviour {
 
     GameObject player;
     public GameObject dummyCam;
+    public Transform lerp1;
+    bool donLerping = false;
+    float distance;
+    public Transform lookat;
 
 	// Use this for initialization
 	void Start () {
         //player = GameObject.FindGameObjectWithTag("Player");
+        
 	}
 	
 	// Update is called once per frame
@@ -21,8 +26,20 @@ public class CameraFollow : MonoBehaviour {
     {
         if (!flag)
         {
+
             transform.position = dummyCam.transform.position;
             transform.rotation = dummyCam.transform.rotation;
+        }
+    }
+
+    public void lerp()
+    {
+        if(!donLerping)
+        {
+
+            transform.position =  Vector3.Lerp(transform.position, lerp1.position,0.05f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, lerp1.rotation, 0.05f);
+            //donLerping = true;
         }
     }
 }
